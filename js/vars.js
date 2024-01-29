adfc41d9313a00e2009c14ff6fc6df011f63e844
SrJohn963 <srjohn963@gmail.com>
2024-01-29 13:31:10 -0300

    inicio para dar responsividade

---
+++ js/vars.js
@@ -0,0 +1,35 @@
+let celulares = `
+<div class="main-section-div-links-container">
+    <div class="links-container-verMais verMais-section-1">
+        <a href="#">Ver mais produtos</a>
+    </div>
+    <div class="links-container-cards cards-section-1">
+        <a href="#" class="link-card">
+            <img src="/img/13.png" alt="">
+            <p>Pc Gamer ZTX</p>
+            <p>R$ 1,234.89</p>
+        </a>
+        <a href="#" class="link-card"><img src="/img/2010.webp" alt="">
+            <p>Pc Gamer ZTX</p>
+            <p>R$ 1,234.89</p>
+        </a>
+        <a href="#" class="link-card"><img src="/img/640.png" alt="">
+            <p>Pc Gamer ZTX</p>
+            <p>R$ 1,234.89</p>
+        </a>
+        <a href="#" class="link-card"><img src="/img/67.png" alt="">
+            <p>Pc Gamer ZTX</p>
+            <p>R$ 1,234.89</p>
+        </a>
+        <a href="#" class="link-card"><img src="/img/410-1.png" alt="">
+            <p>Pc Gamer ZTX</p>
+            <p>R$ 1,234.89</p>
+        </a>
+        <a href="#" class="link-card"><img src="/img/978.png" alt="">
+            <p>Pc Gamer ZTX</p>
+            <p>R$ 1,234.89</p>
+        </a>
+    </div>
+</div>
+<img src="/img/67.png" alt="Computador-banner-section-1" class="img-div-container">
+`
